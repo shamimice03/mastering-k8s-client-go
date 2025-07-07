f95442b67d0504d010eebd489659be23dd8aabfb
shamimice03 <shamimice03@gmail.com>
2025-07-08 00:41:00 +0900

    added SharedInformerFactory

--- 07_shared_informer_factory/main.go
+++ 07_shared_informer_factory/main.go
@@ -3,25 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1" // This imports Pod, Service, etc.
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/klog"
 )
 
 // createClientset creates and returns a Kubernetes clientset
-func createClientset() *kubernetes.Clientset {
+func createClientSet() *kubernetes.Clientset {
 	// Get home directory for kubeconfig path
 	home, err := os.UserHomeDir()
 	if err != nil {
-		klog.Fatalf("Failed to get home directory: %v", err)
+		log.Fatalf("Failed to get home directory: %v", err)
 	}
 	// Parse kubeconfig flag
 	kubeconfig := flag.String("kubeconfig", filepath.Join(home, "/.kube/config"), "location of kubeconfig file")
@@ -29,47 +29,52 @@ func createClientset() *kubernetes.Clientset {
 	// Build config from kubeconfig file
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		klog.Fatalf("Failed to build config: %v", err)
+		log.Fatalf("Failed to build config: %v", err)
 	}
 	// Create clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		klog.Fatalf("Failed to create clientset: %v", err)
-
+		log.Fatalf("Failed to create clientset: %v", err)
 	}
 	return clientset
 }
 
 func main() {
-	clientset := createClientset()
+	// Create client
+	clientset := createClientSet()
 
-	//Single shared factory
+	// Single factory for all informers
 	factory := informers.NewSharedInformerFactory(clientset, time.Second*30)
 
-	// Controllers using same factory
-	startPodMonitor(factory)
-	startDeploymentManager(factory)
-	startPodScaler(factory)
+	// Setup multiple informers using same factory
+	setupPodMonitor(factory)
+	setupDeploymentMonitor(factory)
+	setupPodUpdateMonitor(factory)
 
 	// Start all informers at once
 	stopCh := make(chan struct{})
 	factory.Start(stopCh)
 	factory.WaitForCacheSync(stopCh)
 	<-stopCh
-
 }
 
-func startPodMonitor(factory informers.SharedInformerFactory) {
-	podInformer := factory.Core().V1().Pods() // Gets shared Pod informer
+// Controller 1: Pod Monitor
+func setupPodMonitor(factory informers.SharedInformerFactory) {
+	podInformer := factory.Core().V1().Pods()
 
 	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			pod := obj.(*corev1.Pod)
 			fmt.Printf("[Monitor] Pod added: %s\n", pod.Name)
 		},
+		DeleteFunc: func(obj interface{}) {
+			pod := obj.(*corev1.Pod)
+			fmt.Printf("[Monitor] Pod deleted: %s\n", pod.Name)
+		},
 	})
 }
 
+// Controller 2: Deployment Manager
 // Deployment Manager by implementing cache.ResourceEventHandler
 type DeploymentHandler struct{}
 
@@ -79,14 +84,14 @@ func (h *DeploymentHandler) OnAdd(obj interface{}, isInInitialList bool) {
 }
 
 func (h *DeploymentHandler) OnUpdate(oldObj, newObj interface{}) {
-	// Empty implementation
+	// Implementation
 }
 
 func (h *DeploymentHandler) OnDelete(obj interface{}) {
-	// Empty implementation
+	// Implementation
 }
 
-func startDeploymentManager(factory informers.SharedInformerFactory) {
+func setupDeploymentMonitor(factory informers.SharedInformerFactory) {
 	deploymentInformer := factory.Apps().V1().Deployments()
 
 	handler := &DeploymentHandler{}
@@ -94,42 +99,15 @@ func startDeploymentManager(factory informers.SharedInformerFactory) {
 
 }
 
-// func startDeploymentManager(factory informers.SharedInformerFactory) {
-//     deploymentInformer := factory.Apps().V1().Deployments() // Different resource type
-
-//     deploymentInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-//         AddFunc: func(obj interface{}) {
-//             deployment := obj.(*appsv1.Deployment)
-//             fmt.Printf("[Manager] Deployment added: %s\n", deployment.Name)
-//         },
-//     })
-// }
-
-// Controller 1: Pod Monitor
-// func startPodMonitor(factory informers.SharedInformerFactory) {
-// 	podInformer := factory.Core().V1().Pods() // Gets shared Pod informer
-
-// 	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-// 		AddFunc: func(obj interface{}) {
-// 			pod := obj.(*corev1.Pod)
-// 			fmt.Printf("[Monitor] Pod added: %s\n", pod.Name)
-// 		},
-// 	})
-// }
-
-// // Controller 2: Pod Scaler (uses SAME Pod informer as Controller 1)
-func startPodScaler(factory informers.SharedInformerFactory) {
+// Controller 3: Pod Update Monitor (uses SAME Pod informer as Controller 1)
+func setupPodUpdateMonitor(factory informers.SharedInformerFactory) {
 	podInformer := factory.Core().V1().Pods() // Gets the SAME shared Pod informer
 
 	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		UpdateFunc: func(oldObj, newObj interface{}) {
 			pod := newObj.(*corev1.Pod)
-			fmt.Printf("[Scaler] Pod updated: %s\n", pod.Name)
-			// Scaling logic here
-		},
-		DeleteFunc: func(obj interface{}) {
-			pod := obj.(*corev1.Pod)
-			fmt.Printf("[Scaler] Pod deleted: %s\n", pod.Name)
+			fmt.Printf("[PodUpdateMonitor] Pod updated: %s\n", pod.Name)
+			// logic here
 		},
 	})
 }
